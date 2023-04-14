2645fc7a7916f67eb0006f4ddee1998fa3f877ea
baixxqPro14 <baixxq@163.com>
2023-04-15 00:46:38 +0800

    Site updated: 2023-04-15 00:46:36

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "012",
+      "background": "5",
+      "tmpColor": "4f8ed6",
+      "tmpSize": "16",
+      "cityColor": "4f8ed6",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "492556e6ddd045b2913f833b9b85b62a"
+    }
+  }
